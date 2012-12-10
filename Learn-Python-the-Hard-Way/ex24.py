450df249b538b396f892eca13ba2f14684795bd6
Joe G <joe.granados@hotmail.com>
2012-12-09 16:03:06 -0800

    ex24 sd

--- Learn-Python-the-Hard-Way/ex24.py
+++ Learn-Python-the-Hard-Way/ex24.py
@@ -1,7 +1,7 @@
 print "Let's practice everything."
 print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.'
 
-poem = """\tThe lovely world
+poem = """  The 'lovely" world
 with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
