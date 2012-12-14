39c91a9a6cfcd4fffc598dd392860d65cd936452
Joe G <joe.granados@hotmail.com>
2012-12-14 02:23:23 -0800

    ex33 sd

--- Learn-Python-the-Hard-Way/ex33.py
+++ Learn-Python-the-Hard-Way/ex33.py
@@ -1,13 +1,14 @@
-i = 0
-numbers = []
+def numbers(n, ia):
+  i = 0
+  numbers = []
+  for i in range(0, n, ia):
+    print "At the top i is %d" % i
+    numbers.append(i)
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+  return numbers
 
-while i < 6:
-  print "At the top i is %d" % i
-  numbers.append(i)
-
-  i = i + 1
-  print "Numbers now: ", numbers
-  print "At the bottom i is %d" % i
+numbers = numbers(5, 2)
 
 print "The numbers: "
 
